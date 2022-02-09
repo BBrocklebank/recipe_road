3a408072b9a89879569c2cce62c20ea38d29967a
Benjamin Brocklebank <82475611+BBrocklebank@users.noreply.github.com>
2022-02-09 19:01:04 +0000

    Feat: Add main app run function

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+"""
+Main app for site, linking Jinga templates to python functions.
+Function then interact with MongoDB
+"""
+
+
 import os
 from flask import (
     Flask, flash, render_template,
@@ -5,3 +11,16 @@ from flask import (
 if os.path.exists('env.py'):
     import env
 
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'hello world'
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP", "127.0.0.1"),
+            port=int(os.environ.get("PORT", 5000)),
+            debug=True)
