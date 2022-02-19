b3941a51e85bc80d33ec94ecc6c63280badb7fd2
Benjamin Brocklebank <82475611+BBrocklebank@users.noreply.github.com>
2022-02-19 13:00:29 +0000

    Refactor: Moved Js from base html to script.js

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,19 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: 'right'});
     $('.modal').modal();
     });
+
+ // Opens modal to display flash messages
+ let formMessages = $('.flash_message').text();
+ $(document).ready(function() {
+     if (formMessages) {
+         $('.modal').modal('open');
+     }
+
+     // Sets text colour based on flash message
+     if (formMessages == 'Registration Succesful!') {
+         $('.flash_message').css('color', 'green');
+     } else {
+         $('.flash_message').css('color', 'red');
+     }
+
+ });
