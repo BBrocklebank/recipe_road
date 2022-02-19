193687ec2b0ddcdb3b8dc8f4f53317260eefa360
Benjamin Brocklebank <82475611+BBrocklebank@users.noreply.github.com>
2022-02-19 15:53:55 +0000

    Feature: Change modal form action depending on nav button selected

--- static/js/script.js
+++ static/js/script.js
@@ -1,21 +1,35 @@
 // Inititialise Materialize js components
-$(document).ready(function(){
-    $('.sidenav').sidenav({edge: 'right'});
-    $('.modal').modal();
+$(document).ready(function () {
+    $('.sidenav').sidenav({
+        edge: 'right'
     });
+    $('.modal').modal();
+});
 
- // Opens modal to display flash messages
- let formMessages = $('.flash_message').text();
- $(document).ready(function() {
-     if (formMessages) {
-         $('.modal').modal('open');
-     }
+// Opens modal to display flash messages
+let formMessages = $('.flash_message').text();
+$(document).ready(function () {
+    if (formMessages) {
+        $('.modal').modal('open');
+    }
 
-     // Sets text colour based on flash message
-     if (formMessages == 'Registration Succesful!') {
-         $('.flash_message').css('color', 'green');
-     } else {
-         $('.flash_message').css('color', 'red');
-     }
+    // Sets text colour based on flash message
+    if (formMessages == 'Registration Succesful!') {
+        $('.flash_message').css('color', 'green');
+    } else {
+        $('.flash_message').css('color', 'red');
+    }
+});
 
- });
+// Alters form route depending on nav button selected
+ $( ".register_btn" ).click(function() {
+    $('.form').attr('action', '{{ url_for("register") }}');
+  });
+
+  $( ".login_btn" ).click(function() {
+    $('.form').attr('action', '{{ url_for("login") }}');
+  });
+
+  $( ".logout_btn" ).click(function() {
+    $('.form').attr('action', '{{ url_for("logout") }}');
+  });
