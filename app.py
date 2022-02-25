d8665079457e4739e20396ba4aa1a7d08959948f
Benjamin Brocklebank <82475611+BBrocklebank@users.noreply.github.com>
2022-02-25 16:13:30 +0000

    Bug: Fixed incorrect img src url

--- app.py
+++ app.py
@@ -309,6 +309,6 @@ def search():
 
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP", "127.0.0.1"),
+    app.run(host=os.environ.get("IP", "0.0.0.0"),
             port=int(os.environ.get("PORT", 5000)),
             debug=True)
